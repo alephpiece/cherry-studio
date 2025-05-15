61f424898e654a9be008f85e626b560931d2e360
one <wangan.cs@gmail.com>
2025-05-15 15:04:58 +0800

    fix: migrate version and lint error

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -46,7 +46,7 @@ const persistedReducer = persistReducer(
   {
     key: 'cherry-studio',
     storage,
-    version: 101,
+    version: 102,
     blacklist: ['runtime', 'messages', 'messageBlocks'],
     migrate
   },
